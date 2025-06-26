a4d91d47ec7d74f9b9bb30ef263c314f8f44a8d1
AlyonaCher <alyonka.home@gmail.com>
2025-06-26 14:35:50 -0400

    uncommented dependency

--- playwright.config.ts
+++ playwright.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
         //headless: false,
       },
       teardown: 'cleanup test data',
-      //dependencies: ['auth-setup'],
+      dependencies: ['auth-setup'],
     },
   ],
 
